1dcd0585cb6fa2af4ba616bf8b6126126dcb1cea
Jihunkim95 <darak551@naver.com>
2024-04-12 16:32:25 +0900

    [Init]: 프로젝트 생성

---
+++ Today/Today/Models/Reminder.swift
@@ -0,0 +1,8 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by 김지훈 on 2024/04/12.
+//
+
+import Foundation
---
+++ Today/Today/ReminderListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by 김지훈 on 2024/04/12.
+//
+
+import UIKit
+
+class ReminderListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
